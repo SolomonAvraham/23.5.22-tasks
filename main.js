86af7f9cbb2360080f1dce0623c6a5886e3659d7
SolomonAvraham <Selmon_a@hotmail.com>
2022-05-23 19:11:35 +0300

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 49. צרו פונקציה שמדפיסה לקונסול את התאריך והשעה בעת הפעלתה.
+// 163. צרו פונקציה שמדפיסה לקונסול את התאריך והשעה בעת הפעלתה.
 
 function printDateAndTime() {
   var myDate = new Date();
@@ -6,5 +6,90 @@ function printDateAndTime() {
 }
 printDateAndTime();
 
-// 50. צרו פונקציה המקבלת תאריך כסטרינג ומדפיסה לקונסול אובייקט
+// 164. צרו פונקציה המקבלת תאריך כסטרינג ומדפיסה לקונסול אובייקט
 // Date עם אותו תאריך שהתקבל בפונקציה.
+
+function getString(myDate) {
+  myDate = new Date();
+  console.log(myDate);
+}
+getString("may 23 2022");
+
+// 165. צרו פונקציה הקולטת מהמשתמש תאריך כסטרינג ומדפיסה לקונסול אובייקט של
+// Date.
+
+// function getNumFromUser() {
+//     var userInput = prompt("Enter Date:",'an example:"may 23 2022"');
+//     console.log(new Date(userInput));
+// }
+// getNumFromUser();
+
+// 166. צרו פונקציה הקולטת מהמשתמש תאריך כסטרינג ומחזיררה את החודש.
+
+// function getDateAndPrintMonth() {
+//     var userDate = new Date(prompt("Enter Date:"));
+//     return userDate.getMonth();
+// }
+// getDateAndPrintMonth();
+
+// 167 צרו פונקציה הקולטת מהמשתמש תאריך כסטרינג ומחזירה את השעה.
+
+// function getUserDateAndReturnHour() {
+//     var userDate = new Date(prompt("Enter a Date:"));
+//     return userDate.getHours();
+// }
+
+// getUserDateAndReturnHour();
+
+// 168 צרו פונקציה הקולטת מהמשתמש תאריך כסטרינג ומחזירה את השנה.
+
+// function getUserDateAndReturnYear() {
+//     var userDate = new Date(prompt("Enter a Date:"));
+//     return userDate.getFullYear();
+// }
+// getUserDateAndReturnYear();
+
+// 169 צרו פונקציה הקולטת מהמשתמש תאריך כסטרינג ומחזירה את הדקות.
+
+// function getUserDateAndReturnMinutes() {
+//     var userDate = new Date(prompt("type a random date"));
+//     return userDate.getMinutes();
+// }
+// getUserDateAndReturnMinutes();
+
+// 171 צרו פונקציה הקולטת מהמשתמש תאריך כסטריג ומחזירה את הגיל שלו.
+
+// function getUserDateAndReturnAge() {
+
+//   var userDate = new Date(prompt("Enter a Date:"));
+//   return new Date().getFullYear() - new Date(userDate).getFullYear();
+// }
+
+// getUserDateAndReturnAge();
+
+// 172 צרו פונקציה הקולטת מהמשתמש תאריך כסטרינג, אם המשתמש מעל 18 היא מדפיסה alert
+//  שהוא רשאי להיכנס אחרת מדפיסה הודעה שהמשתמש לא רשאי.
+
+// function getUserDateAndPrint() {
+//   var userDate = new Date(prompt("Enter your BirthDate:"));
+//   var userAge = new Date().getFullYear() - new Date(userDate).getFullYear();
+//   if (userAge > 18) {
+//     alert("Allow to enter!");
+//   } else {
+//     alert("To small to enter!");
+//   }
+// }
+// getUserDateAndPrint();
+
+// 173 צרו פונקציה שיוצרת אובייקט של Date
+//  ומדפיסה למסך את המיקום של המשתמש.
+
+// function getUserDateAndPrintLocation() {
+//   var myDate = new Date();
+//   document.write(GeolocationCoordinates(myDate, myDate));
+// }
+// getUserDateAndPrintLocation();
+
+// לא פתור!
+
+
