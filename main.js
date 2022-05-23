3af46d7882a0c0d230b5149002a1919841871358
SolomonAvraham <Selmon_a@hotmail.com>
2022-05-23 11:23:50 +0300

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+// 49. צרו פונקציה שמדפיסה לקונסול את התאריך והשעה בעת הפעלתה.
+
+function printDateAndTime() {
+  var myDate = new Date();
+  console.log(myDate);
+}
+printDateAndTime();
+
+// 50. צרו פונקציה המקבלת תאריך כסטרינג ומדפיסה לקונסול אובייקט
+// Date עם אותו תאריך שהתקבל בפונקציה.
